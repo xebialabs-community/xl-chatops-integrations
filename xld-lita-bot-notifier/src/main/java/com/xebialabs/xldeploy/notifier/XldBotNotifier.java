634addd1fb91eab4fa6f32f78de2cf16e1510dbc
Martin Van Vliet <martin@Riverrun.xebialabs.com>
2016-04-11 17:26:54 +0200

    Added logging. Added configurability via xld-bot.conf

--- xld-lita-bot-notifier/src/main/java/com/xebialabs/xldeploy/notifier/XldBotNotifier.java
+++ xld-lita-bot-notifier/src/main/java/com/xebialabs/xldeploy/notifier/XldBotNotifier.java
@@ -1,5 +1,10 @@
 package com.xebialabs.xldeploy.notifier;
 
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
@@ -16,10 +21,28 @@ import com.xebialabs.deployit.engine.spi.execution.TaskExecutionStateEvent;
 
 import nl.javadude.t2bus.Subscribe;
 
+/**
+ * This class is a plugin for XL Deploy that posts task status updates to a REST endpoint, such
+ * as the endpoint included in the XebiaLabs lita bot. 
+ */
 @DeployitEventListener
 public class XldBotNotifier implements ExecutionStateListener {
 	
-	private static String BOT_URL = "http://localhost:8080";
+	private static final Logger LOG = LoggerFactory.getLogger(XldBotNotifier.class);
+	
+	private static String DEFAULT_BOT_URL = "http://localhost:8080";
+	private String botURL = DEFAULT_BOT_URL;
+	
+	public XldBotNotifier() {
+		try {
+			Properties props = new Properties();
+			props.load(ClassLoader.getSystemResourceAsStream("xld-bot.conf"));
+			botURL = props.getProperty("bot.url", DEFAULT_BOT_URL);
+		} catch(Exception e) {
+			// ignore
+		}
+		LOG.debug("Using bot URL " + botURL);
+	}
 	
 	@Subscribe
 	public void log(TaskStartedEvent event) throws UnirestException {
@@ -52,21 +75,28 @@ public class XldBotNotifier implements ExecutionStateListener {
 	}
 	
 	private void postNotification(String taskId, String status) throws UnirestException {
-		HttpResponse<String> jsonResponse = Unirest.post(BOT_URL + "/task/" + taskId + "/" + status)
+		HttpResponse<String> jsonResponse = Unirest.post(botURL + "/task/" + taskId + "/" + status)
 				  .header("Content-type", "application/json")
 				  .asString();
 		if (jsonResponse.getStatus() != 200) {
-			System.out.println("Error invoking bot: " + jsonResponse.getStatus());
+			LOG.debug("Failed to push event to bot, status code " + jsonResponse.getStatusText());
 		}
 	}
 
 	@Override
 	public void stepStateChanged(StepExecutionStateEvent event) {
+		try {
+			LOG.debug("Task " + event.task().getId() + " step state changed, pushing event to bot URL " + botURL);
+			postNotification(event.task().getId(), event.currentState().toString());
+		} catch (UnirestException e) {
+			// Silently fail
+		}
 	}
 
 	@Override
 	public void taskStateChanged(TaskExecutionStateEvent event) {
 		try {
+			LOG.debug("Task " + event.task().getId() + " state changed, pushing event to bot URL " + botURL);
 			postNotification(event.task().getId(), event.currentState().toString());
 		} catch (UnirestException e) {
 			// Silently fail
